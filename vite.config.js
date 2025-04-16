dadbf68e6af93b4dd12274f2a826f0680a63cda9
YeralyKuatuly <yeralykuatuly@gmail.com>
2025-04-17 01:12:13 +0500

    [FIX] css sintax error

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    // Fix CSS minification issues
+    postcss: {
+      plugins: []
+    },
+    preprocessorOptions: {
+      scss: {
+        additionalData: ''
+      }
+    }
+  },
+  build: {
+    cssMinify: 'lightningcss',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          router: ['react-router-dom'],
+        }
+      }
+    }
+  }
 })
